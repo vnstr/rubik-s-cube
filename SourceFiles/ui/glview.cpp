2a49e09e4e0bf0848b54bb0bf3bf255188e5ac57
Ivan Vostrilov <vnstr.main@gmail.com>
2022-12-16 23:41:24 +0600

    GLView preparing

--- SourceFiles/ui/glview.cpp
+++ SourceFiles/ui/glview.cpp
@@ -2,6 +2,7 @@
 
 #include <string>
 
+#include "core/glerror.h"
 #include "core/logger.h"
 
 namespace RCube {
@@ -14,13 +15,22 @@ void GLView::initializeGL() {
   std::string msg;
   msg.reserve(64);
 
+  msg += "GL_VERSION: \"";
+  msg += reinterpret_cast<const char *>(glGetString(GL_VERSION));
+  msg += "\" ";
+
   msg += "GL_RENDERER: \"";
-  msg += reinterpret_cast<const char*>(glGetString(GL_RENDERER));
+  msg += reinterpret_cast<const char *>(glGetString(GL_RENDERER));
   msg += "\"";
   Core::Logger::Log(msg);
+
+  auto error = Core::GLError::Call([]() { glClearColor(0, 1, 0, 0); });
+  if (error) {
+    error.Log(Core::Logger::ToPrefix({__FUNCTION__, "glClearColor"}));
+  }
 }
 
-void GLView::paintGL() {}
+void GLView::paintGL() { glClear(GL_COLOR_BUFFER_BIT); }
 
 void GLView::resizeGL(int w, int h) {}
 
